df57bf4d66a6c28394f22a416976e91d92f3e7f6
Nikolaos Margaris <reverence23@gmail.com>
2019-01-26 22:19:56 +0200

    tidy up

--- index.js
+++ index.js
@@ -3,20 +3,27 @@
 const log = console.log;
 const time = console.time;
 const timeEnd = console.timeEnd;
+const metric = (name, fn) => {
+  time(name);
+  fn();
+  timeEnd(name);
+};
 
 // S2 = [ x^2 | x ε S1, x < 10 ]
 // for every x in S1 and x < 10 then provide 2x
 
-function S1() {
-  let _elem = 0; // initial value
+// This looks like a Set of primitive items i.e numbers
+function S1(initial) {
+  let _val = initial;
 
   this.next = function () {
-    return ++_elem;
+    return ++_val;
   };
 }
 
+// Transformer of items in S1
 function S2(calcFn, condFn) {
-  let _set1 = new S1();
+  let _set1 = new S1(0);  // very restrictive !!!
 
   this.toList = function* () {
     let newItem = _set1.next();
@@ -29,20 +36,26 @@ function S2(calcFn, condFn) {
   };
 }
 
-const s2_a = new S2(x => x*x, x => x < 1000);
+// Generate a Set from another Set of integers
+const evaluator = x => x * x;
+const conditioner = x => x < 1000;  // or 'limiter'
 
-time('for..of');
-const toList = s2_a.toList();
-for (let x of toList) x;
-timeEnd('for..of');
+// Parse using for..of (slower)
+const s2_a = new S2(evaluator, conditioner);
 
-const s2_b = new S2(x => x*x, x => x < 1000);
+metric('for..of', () => {
+  const toList = s2_a.toList();
+  for (let x of toList) x;
+});
+
+// Parse using iterator
+const s2_b = new S2(evaluator, conditioner);
 const iter = s2_b.toList();
 
-time('iterator');
-let res = iter.next();
-while(!res.done) {
-  res.value;
-  res = iter.next();
-}
-timeEnd('iterator')
+metric('iterator', () => {
+  let res = iter.next();
+  while(!res.done) {  // while === 'where'
+    res.value;
+    res = iter.next();
+  }
+});
